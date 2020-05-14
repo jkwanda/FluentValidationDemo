49483d2bec68030ed1ec9c12bb87e45934f6ee16
Justin Kloetzer <justin.kloetzer@gmail.com>
2020-05-14 11:24:17 -0500

    Add project files.

---
+++ DashboardUI/Dashboard.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DashboardUI.Validators;
+using ModelLibrary;
+using FluentValidation.Results;
+
+namespace DashboardUI
+{
+  public partial class Dashboard : Form
+  {
+    BindingList<string> errors = new BindingList<string>();
+
+    public Dashboard()
+    {
+      InitializeComponent();
+
+      errorListBox.DataSource = errors;
+    }
+
+    private void createButton_Click(object sender, EventArgs e)
+    {
+      errors.Clear();
+
+      if (!int.TryParse(accountNumberText.Text, out int accountNumber))
+      {
+        errors.Add("Account Number: Invalid Amount");
+        return;
+      }
+
+      PersonModel person = new PersonModel();
+      person.FirstName = firstNameText.Text;
+      person.LastName = lastNameText.Text;
+      person.AccountNumber = Convert.ToInt32(accountNumberText.Text);
+      person.DateOfBirth = dateOfBirthPicker.Value;
+
+      // Validate data
+      PersonValidator validator = new PersonValidator();
+
+      ValidationResult results = validator.Validate(person);
+
+      if (results.IsValid == false)
+      {
+          foreach (ValidationFailure failure in results.Errors)
+          {
+             errors.Add($"{failure.ErrorMessage }");
+          }
+      }
+      // Insert into the database
+
+      MessageBox.Show("Operation Complete");
+    }
+
+        private void accountBalanceLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DashboardUI/Validators/PersonValidator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using ModelLibrary;
+
+namespace DashboardUI.Validators
+{
+    class PersonValidator : AbstractValidator<PersonModel>
+    {
+
+        public PersonValidator()
+        {
+            RuleFor(p => p.FirstName)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("{PropertyName} is empty.")
+                .Length(2, 50).WithMessage("Length ({TotalLength}) of {PropertyName} invalid.")
+                .Must(BeValidName).WithMessage("{PropertyName} contains invalid characters.");
+
+            RuleFor(p => p.LastName)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("{PropertyName} is empty.")
+                .Length(2, 50).WithMessage("Length ({TotalLength}) of {PropertyName} invalid.")
+                .Must(BeValidName).WithMessage("{PropertyName} contains invalid characters.");
+
+            RuleFor(p => p.DateOfBirth)
+                .Must(BeValidAge).WithMessage("Invalid {PropertyName}.");
+
+            RuleFor(p => p.AccountNumber)
+                .Must(BeValidAccountNumber).WithMessage("{PropertyName} is too low.");
+        }
+
+        protected bool BeValidName(string name)
+        {
+            name = name.Replace(" ", "");
+            name = name.Replace("-", "");
+            return name.All(Char.IsLetter);
+        }
+
+        protected bool BeValidAge(DateTime date)
+        {
+            int currentYear = DateTime.Now.Year;
+            int dobYear = date.Year;
+
+            if(dobYear <= currentYear && dobYear > (currentYear - 120))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        protected bool BeValidAccountNumber(int accountNumber)
+        {
+            if(accountNumber >= 100)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
